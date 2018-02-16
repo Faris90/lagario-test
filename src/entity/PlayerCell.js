05db0da103db1af808301a34f6420f62c061a443
( ͡° ͜ʖ ͡°) Fs unlimited <faris.soliman@yahoo.com>
2018-02-16 09:59:03 -0600

    Update PlayerCell.js

--- src/entity/PlayerCell.js
+++ src/entity/PlayerCell.js
@@ -2,11 +2,10 @@ var Cell = require('./Cell');
 
 function PlayerCell() {
     Cell.apply(this, Array.prototype.slice.call(arguments));
-     if(Math.floor(Math.random() * 10) + 1  == 10
-    this.cellType = 2;
-	} else 
-	this.cellType = 0
-	}
+    if(Math.floor(Math.random() * 10) + 1  == 10){
+    this.celltype = 2;
+      } else
+    this.celltype = 0
 	
 	}
 
